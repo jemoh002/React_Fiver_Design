60c6209624488879dcfbdad5f056b3a1b9ecceac
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-22 15:39:26 +0300

    completed the message page design

--- fiverr_design/src/pages/message/Message.jsx
+++ fiverr_design/src/pages/message/Message.jsx
@@ -19,7 +19,52 @@ function Message() {
               dolore quaerat, est aperiam. Delectus ad reiciendis doloribus magnam nihil fugit excepturi perspiciatis qui nobis.
             </p>
           </div>
+
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente fugiat repellendus minima veniam laborum molestias
+              dolore quaerat, est aperiam. Delectus ad reiciendis doloribus magnam nihil fugit excepturi perspiciatis qui nobis.
+            </p>
+          </div>
+
+
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente fugiat repellendus minima veniam laborum molestias
+              dolore quaerat, est aperiam. Delectus ad reiciendis doloribus magnam nihil fugit excepturi perspiciatis qui nobis.
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente fugiat repellendus minima veniam laborum molestias
+              dolore quaerat, est aperiam. Delectus ad reiciendis doloribus magnam nihil fugit excepturi perspiciatis qui nobis.
+            </p>
+          </div>
+
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente fugiat repellendus minima veniam laborum molestias
+              dolore quaerat, est aperiam. Delectus ad reiciendis doloribus magnam nihil fugit excepturi perspiciatis qui nobis.
+            </p>
+          </div>
+
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente fugiat repellendus minima veniam laborum molestias
+              dolore quaerat, est aperiam. Delectus ad reiciendis doloribus magnam nihil fugit excepturi perspiciatis qui nobis.
+            </p>
+          </div>
         </div>
+
+        <hr />
+
+        
         <div className="write">
           <textarea name="" placeholder='write a message' id="" cols="30" rows="10"></textarea>
           <button>Send</button>
