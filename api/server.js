deca692cd564b7868d740bc583d6267a99436224
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-24 10:35:56 +0300

    Added some configurations and changes in server script

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
-import userRoute from "./routes/user.route"
+import cookieParser from "cookie-parser";
+
+import userRoute from "./routes/user.route.js"
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+import authRoute from "./routes/auth.route.js"
 
 const app = express()
 dotenv.config()
@@ -18,12 +26,27 @@ const connect = async () => {
     }
 };
 
+app.use(express.json())
+app.use(cookieParser());
 
+
+app.use("/api/auths", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/conversations", conversationRoute)
+app.use("/api/messages", messageRoute)
+app.use("/api/reviews", reviewRoute)
 
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+
+    return res.status(errorStatus).send(errorMessage);
+})
 
 app.listen(8800, () => {
     connect()
-    console.log(`Server is running`)
+    console.log(`Server is running on port 8800`)
 })
