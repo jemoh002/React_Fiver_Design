29c55b58be05297cebc2cba1a27ce29dacf4ac30
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-26 21:01:25 +0300

    some changes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { deleteUser } from '../controllers/user.controller';
+import { deleteUser, getUser } from '../controllers/user.controller';
 import { verifyToken } from '../middleware/jwt';
 
 const router = express.Router()
 
 router.delete("/:id", verifyToken, deleteUser)
+
+router.get("/:id",verifyToken, getUser)
 
 export default router;
