5be35ae197512d71b7d58b8d0ad48cb1829127cc
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-26 21:54:55 +0300

    made some changes to separate reviews code from gig into reviews and review -react, together with their stylings

--- fiverr_design/src/pages/Gig/Gig.jsx
+++ fiverr_design/src/pages/Gig/Gig.jsx
@@ -4,6 +4,7 @@ import { Slider } from "infinite-react-carousel/lib";
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import newRequest from '../../utils/newRequest';
+import Reviews from '../../components/reviews/Reviews';
 
 function Gig() {
   const {id} = useParams()
@@ -85,7 +86,7 @@ function Gig() {
               {
                 isLoadingUser ? "loading"
                 : errorUser ? "Something went wrong!"
-                : <div className="seller">
+                : (<div className="seller">
                     <h2>About the seller</h2>
                     <div className="user">
                       <img src={dataUser.img || "/img/noavatar.jpg"} alt="" />
@@ -144,170 +145,56 @@ function Gig() {
                       </div>
                       
                     </div>
-                
+                )
               }
 
-          <div className="reviews">
-            <h2>Reviews</h2>
-            <div className="item">
-              <div className="user">
-                <img className='pp' src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <div className="info">
-                  <span>John Doe</span>
-                  <div className="country">
-                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
-                    <span>United States</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="stars">
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <span>5</span>
-              </div>
-              <p>
-                I just want to say that art_with_ai was the first, and after
-                this, the only artist Ill be using on Fiverr. Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-
-            <div className="item">
-              <div className="user">
-                <img className='pp' src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <div className="info">
-                  <span>John Doe</span>
-                  <div className="country">
-                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
-                    <span>United States</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="stars">
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <span>5</span>
-              </div>
-              <p>
-                I just want to say that art_with_ai was the first, and after
-                this, the only artist Ill be using on Fiverr. Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            
-
-            <div className="item">
-              <div className="user">
-                <img className='pp' src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <div className="info">
-                  <span>John Doe</span>
-                  <div className="country">
-                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt="" />
-                    <span>United States</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="stars">
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <img src="/img/star.png" alt="" />
-                    <span>5</span>
-              </div>
-              <p>
-                I just want to say that art_with_ai was the first, and after
-                this, the only artist Ill be using on Fiverr. Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-
-          </div>
-        </div>
-        <div className="right">
-          <div className="price">
-            <h2>
-              {data.shortTitle}
-            </h2>
-            <h3>$ {data.price}</h3>
-          </div>
-          <p>
-            {data.shortDesc}
-          </p>
-          <div className="details">
-            <div className="item">
-                <img src="/img/clock.png" alt="" />
-                <span>{data.DeliveryDate} days Delivery</span>
-            </div>
-
-            <div className="item">
-                <img src="/img/recycle.png" alt="" />
-                <span>{data.revisionNumber} Revisions</span>
+              <Reviews gigId={id} />
+              
             </div>
             
-          </div>
 
-          <div className="features">
-            {
-              data.features.map((feature) => (
-                <div className="item">
+            <div className="right">
+              
+                <div className="price">
+                  <h2>
+                    {data.shortTitle}
+                  </h2>
+                  <h3>$ {data.price}</h3>
+                </div>
+                <p>
+                  {data.shortDesc}
+                </p>
+                <div className="details">
+                  <div className="item">
+                      <img src="/img/clock.png" alt="" />
+                      <span>{data.DeliveryDate} days Delivery</span>
+                  </div>
 
-                  <img src="/img/greencheck.png" alt="" />
+                  <div className="item">
+                      <img src="/img/recycle.png" alt="" />
+                      <span>{data.revisionNumber} Revisions</span>
+                  </div>
                   
-                  <span>{feature}</span>
-                  
-              </div>
-              ))
-            }
-            
-          </div>
+                </div>
 
-          <button>Continue</button>
-        </div>
+                <div className="features">
+                  {
+                    data.features.map((feature) => (
+                      <div className="item">
+
+                        <img src="/img/greencheck.png" alt="" />
+                        
+                        <span>{feature}</span>
+                        
+                    </div>
+                    ))
+                  }
+                  
+                </div>
+
+                <button>Continue</button>
+            </div>
+            
       </div>}
     </div>
   )
