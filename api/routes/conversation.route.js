052e467867bfee1e41d10a7116e07ff9eb2a20b2
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-27 23:24:25 +0300

    added creaetConversation, getConversation, and getConversations routes

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,6 +1,13 @@
 import express from 'express'
-
 const router = express.Router()
+
+import {verifyToken} from "../middleware/jwt"
+import { createConversation, getConversations, getSingleConversation, updateConversation } from '../controllers/conversation.controller';
+
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 
 export default router;
