c1f9a12b6528935aa3125b85c78c722bf3c9e506
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-27 14:58:07 +0300

    added createOrders and getOrders routes in server api

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import {verifyToken} from "../middleware/jwt"
+import { createOrders, getOrders } from '../controllers/order.controller'
 
 const router = express.Router()
+
+router.post("/:gigId", verifyToken, createOrders)
+router.get("/", verifyToken, getOrders)
 
 
 export default router;
