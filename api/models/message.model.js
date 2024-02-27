216ca435c5e9debe69d07774c00f9aec61f5cb56
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-27 14:25:29 +0300

    added some minor changes to the models, imported Schema

--- api/models/message.model.js
+++ api/models/message.model.js
@@ -1,4 +1,5 @@
 import mongoose, { mongo } from "mongoose";
+const { Schema } = mongoose;
 
 const messageSchema = new Schema({
     ConversationId: {
--- api/models/review.model.js
+++ api/models/review.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
     gigId: {
