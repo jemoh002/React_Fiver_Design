3bbb5a92544b8d17ad021503d804cab509bc8365
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-27 14:23:04 +0300

    created review routes and the controllers

--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -1,6 +1,12 @@
 import express from 'express'
 
+import {verifyToken} from "../middleware/jwt"
+import { createReview, deleteReview, getReview } from '../controllers/review.controller'
+
 const router = express.Router()
 
+router.post("/", verifyToken, createReview)
+router.get("/:gigId", getReview)
+router.delete("/:id", deleteReview)
 
 export default router;
