4dc8d317c3548c4177104b200466c88afcbcdb74
jemoh002 <jamesnyajuaya@gmail.com>
2024-02-28 06:13:39 +0300

    added createMessage and getMessage routes

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,6 +1,10 @@
 import express from 'express'
 
 const router = express.Router()
+import {verifyToken} from "../middleware/jwt"
+import { createMessage, getMessages } from '../controllers/message.controller'
 
+router.post("/", verifyToken, createMessage) // createMessage
+router.get("/:id", verifyToken, getMessages) //get messages
 
 export default router;
