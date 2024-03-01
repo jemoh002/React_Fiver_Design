80be21a00a2dc46278092f9c3fef259ab2d81a75
jemoh002 <jamesnyajuaya@gmail.com>
2024-03-01 23:45:41 +0300

    implemented stripe payment route

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {verifyToken} from "../middleware/jwt"
-import { createOrders, getOrders } from '../controllers/order.controller'
+import { getOrders, intent, confirm } from '../controllers/order.controller'
 
 const router = express.Router()
 
-router.post("/:gigId", verifyToken, createOrders)
 router.get("/", verifyToken, getOrders)
+router.post("/create-payment-intent/:id", verifyToken, intent); // intent
+router.put("/", verifyToken, confirm)
 
 
 export default router;
