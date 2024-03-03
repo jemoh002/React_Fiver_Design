4cd9e491f62121f35620ae2a7254f4dfaed8d8a9
jemoh002 <jamesnyajuaya@gmail.com>
2024-03-03 11:49:01 +0300

    configured useReducer

--- fiverr_design/src/pages/add/Add.jsx
+++ fiverr_design/src/pages/add/Add.jsx
@@ -1,53 +1,158 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import "./Add.scss"
+import { INITIAL_STATE, gigReducer } from '../../reducers/gigReducer'
+import upload from "../../utils/upload"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import newRequest from "../../utils/newRequest"
+import { useNavigate } from "react-router-dom"
 
 function Add() {
+  const [singleFile, setSingleFile] = useState(undefined)
+  const [files, setFiles] = useState([])
+  const [uploading,setUploading]= useState(false)
+  const [state, dispatch] = useReducer(gigReducer, INITIAL_STATE)
+
+  const handleChange = (e )=> {
+    dispatch({
+      type: "CHANGE_INPUT",
+      payload: {name: e.target.name, value:e.target.value},
+    })
+  }
+
+  const handleFeature = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: e.target[0].value,
+    })
+  };
+
+  const handleUpload = async () => {
+    setUploading(true)
+    try {
+      const cover = await upload(singleFile)
+
+      const images = await Promise.all(
+        [...files].map(async (file) => {
+          const url = await upload(file);
+          return url
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } })
+      
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  console.log(state)
+
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+  
+  const mutation = useMutation({
+    mutationFn: (gig) => {
+      return newRequest.post("/gigs", gig)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"])
+    }
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    mutation.mutate(state)
+    navigate("/mygigs")
+  }
+
   return (
     <div className='add'>
       <div className="container">
         <h1>Add New Gig</h1>
         <div className="sections">
           <div className="left">
+
             <label htmlFor="">Title</label>
-            <input type="text" placeholder="e.g. I will do something I'm really good at" />
+            <input type="text" name='title' placeholder="e.g. I will do something I'm really good at" onChange={handleChange} />
+
             <label htmlFor="">Category</label>
-            <select name="cats" id="cats">
+            <select name="cat" id="cat" onChange={handleChange}>
               <option value="design">Design</option>
               <option value="design">Web Development</option>
               <option value="design">Animation</option>
               <option value="design">Music</option>
             </select>
 
-            <label htmlFor="">Cover Image</label>
-            <input type="file" />
-            <label htmlFor="">Upload images</label>
-            <input type="file" multiple/>
+            <div className="images">
+              <div className="imagesInput">
+                
+                <label htmlFor="">Cover Image</label>
+                <input type="file" onChange={e=>setSingleFile(e.target.files[0])}/>
+
+                <label htmlFor="">Upload images</label>
+                <input type="file" multiple onChange={e => setFiles(e.target.files)} />
+                
+              </div>
+
+              <button onClick={handleUpload}>{ uploading ? "uploading" : "Upload"}</button>
+              
+            </div>
+
+            
             <label htmlFor="">Description</label>
-            <textarea name=""
+            <textarea
+              name="desc"
               id=""
               cols="30"
               rows="16"
-              placeholder='Brief description to introduce your service to customers'></textarea>
-            <button>Create</button>
+              placeholder='Brief description to introduce your service to customers'
+              onChange={handleChange}></textarea>
+            
+            <button onClick={handleSubmit}>Create</button>
                         
           </div>
 
           <div className="right">
             <label htmlFor="">Service Title</label>
-            <input type="text" placeholder='e.g. One-page web design' />
+            <input name='shortTitle' type="text" placeholder='e.g. One-page web design' onChange={handleChange}/>
+
             <label htmlFor="">Short Description</label>
-            <textarea name="" id="" cols="30" rows="10" placeholder='Short description of your service'></textarea>
+            <textarea name="shortDesc" onChange={handleChange} id="" cols="30" rows="10" placeholder='Short description of your service'></textarea>
+
             <label htmlFor="">Delivery Time(e.g. 3 days)</label>
-            <input type="number" min={1} />
+            <input name='deliveryTime' type="number" min={1} onChange={handleChange} />
+
             <label htmlFor="">Revision Number</label>
-            <input type="number" min={1} />
+            <input name='revisionNumber' type="number" min={1} onChange={handleChange} />
+
             <label htmlFor="">Add features</label>
-            <input type="text" placeholder='e.g. page design' />
-            <input type="text" placeholder='e.g. file uploading' />
-            <input type="text" placeholder='e.g. setting up a domain' />
-            <input type="text" placeholder='e.g. hosting' />
+            <form action="" className='add' onSubmit={handleFeature}>
+              <input type="text" placeholder='e.g. page design' />
+               <button type='submit'>Add</button>
+            </form>
+            
+
+            <div className="addedFeatures">
+              {
+                state?.features?.map((f) => (
+                  <div className="item" key={f}>
+    
+                    <button onClick={() => dispatch({type:"REMOVE_FEATURE", payload:f})}>
+                      {f} 
+                      <span> X</span>
+                    </button>
+
+                 </div>
+                ))
+              }
+              
+            </div>
+              
+            
+
             <label htmlFor="">Price</label>
-            <input type="number" min={1} />
+            <input type="number" min={1} name='price' onChange={handleChange} />
           </div>
         </div>
       </div>
